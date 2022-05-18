ccea7e3bd4cab07a1eec31360b27317173c391f1
jwleesh <jwleesh@gmail.com>
2022-05-19 02:22:10 +0900

    반영~

---
+++ src/main/java/com/jinwoo/book/springboot/web/dto/PostsResponseDto.java
@@ -0,0 +1,23 @@
+package com.jinwoo.book.springboot.web.dto;
+
+import com.jinwoo.book.springboot.domain.posts.Posts;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+public class PostsResponseDto {
+
+    private Long id;
+    private String title;
+    private String content;
+    private String author;
+
+    @Builder
+    public PostsResponseDto(Posts entity) {
+        this.id = entity.getId();
+        this.title = entity.getTitle();
+        this.content = entity.getContent();
+        this.author = entity.getAuthor();
+    }
+}
